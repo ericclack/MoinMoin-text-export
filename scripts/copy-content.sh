2bf6ba5bd8b31db16b40a117440230e433d3b222
Eric Clack <eric@bn7.net>
2024-01-20 11:05:37 +0000

    Get ready for MM to Markdown conversion

--- scripts/copy-content.sh
+++ scripts/copy-content.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-EXPORTPATH=../export
+EXPORTPATH=../export/mm
 
 for file in `./latest-page-revisions.sh`
 do
     pagename=`echo $file | sed -e 's/^.*pages\/\(.*\)\/revisions.*$/\1/g'`
-    # It isn't actually markdown, but will be soon
     echo Copying $file 
-    cp $file $EXPORTPATH/$pagename.md
+    cp $file $EXPORTPATH/$pagename
 done
